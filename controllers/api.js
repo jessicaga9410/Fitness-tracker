482bf1bab3a8050ded737907439c134078577aed
Jessica Galarza <jessicaga941@gmail.com>
2021-11-14 13:49:27 -0500

    updated router code on api.js file

--- controllers/api.js
+++ controllers/api.js
@@ -2,11 +2,11 @@ const router=require ('express').Router();
 const Workout =require('../models/workout.js');
 
 
-router.post('/api/workouts', (req, res)=>{
+router.post('/workouts', (req, res)=>{
     res.sendFile(path.join(__dirname, '../public/api/workout.js'));
 });
 
-router.put('/api/workouts/:id', ({body, params} , res) =>{
+router.put('/workouts/:id', ({body, params} , res) =>{
     res.sendFile(path.join(__dirname, '../public/api/workouts/:id'));
     
 });
