faef19fef9c486b08f1d929b247c57a837535961
Jessica Galarza <jessicaga941@gmail.com>
2021-11-14 13:27:41 -0500

    added server.js file and models folder along with index.js and workout.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express=require('express');
+const logger= require('morgan');
+const mongoose= require('mongoose');
+const controllers= require('./controllers');
+
+const PORT =process.env.PORT || 3000;
+const app =express();
+app.user(logger('dev'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use (express.static('public'));
+
+mongoose.connect('mongodb://localhost/workout', {
+portuseNewUrlParser:true, 
+useFindAndModify:false,
+useUnifiedTopology:true,
+});
+
+app.use(controllers);
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
+
+
+
