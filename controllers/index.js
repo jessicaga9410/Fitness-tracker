e3258e3c3184e8b95b593a4be7249fc206db449c
Jessica Galarza <jessicaga941@gmail.com>
2021-11-14 13:52:28 -0500

    added index.js file in controllers folder

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,9 @@
-const router =
+const router =require('express').Router();
+const views =require ('./views');
+const apiRoutes =require ('./api');
+
+router.use('/', views);
+router.use('./api', apiRoutes);
+
+module.exports=router;
+
